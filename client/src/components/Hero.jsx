16b91840efcc8b43e33cf19e7901e44cd6a55c08
amisuta <rusli.marc@gmail.com>
2025-08-28 21:23:52 -0700

    Fill out hero content with basic styling

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -2,5 +2,29 @@ import "./Hero.css";
 import heroImage from "../assets/apparelpromo-hero.png";
 
 export default function Hero() {
-  return <img className="hero-image" src={heroImage}></img>;
+  return (
+    <section id="hero">
+      <div className="text-container">
+        <h1>Custom Embroidery & Screen Printing Excellence</h1>
+        <p>
+          Professional custom apparel solutions for businesses, teams, and
+          events. From design to delivery, we make your brand stand out.
+        </p>
+				<ul className="core-services">
+					<li><a href="_">Embroidery</a></li>
+					<li><a href="_"></a>Screen Printing</li>
+					<li><a href="_">Digital Transfer</a></li>
+					<li><a href="_">Other Promotional Products</a></li>
+				</ul>
+				<span>Get Free Quote</span>
+				<span>View Our Work</span>
+				<ul className="trust-indicators">
+					<li>Local Hayward Business</li>
+					<li>20+ Years Experience</li>
+					<li>Quality Guaranteed</li>
+				</ul>
+      </div>
+      <img className="hero-image" src={heroImage}></img>
+    </section>
+  );
 }
--- client/src/components/QuickLinks.jsx
+++ client/src/components/QuickLinks.jsx
@@ -2,13 +2,26 @@ import "./QuickLinks.css";
 
 export default function QuickLinks() {
   return (
-    <div className="nav-container">
-        <nav className="responsive-container">
+    <div className="responsive-container">
+      <nav>
         <ul className="quicklinks">
-            <li><a href="_">Custom T-shirts</a></li>
-            <li><a href="_">Create Your Shirt</a></li>
+          <li>
+            <a href="_">Custom T-shirts</a>
+          </li>
+          <li>
+            <a href="_">Create Your Shirt</a>
+          </li>
         </ul>
-        </nav>
+      </nav>
+      <address>
+        <a href="mailto:sales.apparelpromo@gmail.com">
+          sales.apparelpromo@gmail.com
+        </a>
+        <br />
+        <a href="tel:+15104719010">+1 (510) 471-9010</a>
+        <br />
+        <a href="_">780 Sandoval Way Hayward, CA 94544</a>
+      </address>
     </div>
   );
 }
