f45311d2bb072f48c0e643db993777f47ad61174
amisuta <rusli.marc@gmail.com>
2025-08-31 19:01:02 -0700

    swap position of some nav bar items

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,27 +1,20 @@
 import "./Navbar.css";
 import apLogo from "../assets/apparelpromo-logo.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { library } from "@fortawesome/fontawesome-svg-core";
-
-/* import all the icons in Free Solid, Free Regular, and Brands styles */
-import { fas } from "@fortawesome/free-solid-svg-icons";
-import { far } from "@fortawesome/free-regular-svg-icons";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-
-library.add(fas, far, fab);
 
 export default function Navbar() {
   return (
     <div className="nav-container">
       <nav className="topnav responsive-container">
         <img src={apLogo} className="logo-topnav" alt="Apparel Promo logo" />
-        <ul className="nav-items">
-          <li>asdf1</li>
-          <li>asdf2</li>
-          <li>
-            <FontAwesomeIcon icon="fa-solid fa-cart-shopping" />
-          </li>
-        </ul>
+        <address>
+          <a href="mailto:sales.apparelpromo@gmail.com">
+            sales.apparelpromo@gmail.com
+          </a>
+          <br />
+          <a href="tel:+15104719010">+1 (510) 471-9010</a>
+          <br />
+          <a href="_">780 Sandoval Way Hayward, CA 94544</a>
+        </address>
       </nav>
     </div>
   );
--- client/src/components/QuickLinks.jsx
+++ client/src/components/QuickLinks.jsx
@@ -1,9 +1,10 @@
 import "./QuickLinks.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function QuickLinks() {
   return (
     <div className="responsive-container">
-      <nav>
+      <nav className="subnav">
         <ul className="quicklinks">
           <li>
             <a href="_">Custom T-shirts</a>
@@ -12,16 +13,14 @@ export default function QuickLinks() {
             <a href="_">Create Your Shirt</a>
           </li>
         </ul>
+        <ul className="nav-items">
+          <li>asdf1</li>
+          <li>asdf2</li>
+          <li>
+            <FontAwesomeIcon icon="fa-solid fa-cart-shopping" />
+          </li>
+        </ul>
       </nav>
-      <address>
-        <a href="mailto:sales.apparelpromo@gmail.com">
-          sales.apparelpromo@gmail.com
-        </a>
-        <br />
-        <a href="tel:+15104719010">+1 (510) 471-9010</a>
-        <br />
-        <a href="_">780 Sandoval Way Hayward, CA 94544</a>
-      </address>
     </div>
   );
 }
