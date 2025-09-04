c7923af85d77a4d93505f170e443d1190e73a73f
amisuta <rusli.marc@gmail.com>
2025-09-03 18:24:36 -0700

    Style trust bar

--- client/src/components/TrustBar.jsx
+++ client/src/components/TrustBar.jsx
@@ -1,12 +1,22 @@
-import "./TrustBar.css"
+import "./TrustBar.css";
 export default function TrustBar() {
   return (
     <section id="trust-bar">
-      <h2 className="diminished">Trusted by Bay Area Businesses</h2>
-      <li className="trusted-businesses-container">
-        <img src="_" alt="" />
-        <figcaption>Mills High School</figcaption>
-      </li>
+      <h2 className="diminished">Trusted by Bay Area Businesses, Institutions, and Communities</h2>
+      <ul className="trusted-groups-container">
+        <li className="trusted-group">
+          <img src="_" alt="" />
+          <figcaption>Mills High School</figcaption>
+        </li>
+        <li className="trusted-group">
+          <img src="_" alt="" />
+          <figcaption>Glad Tidings Church</figcaption>
+        </li>
+        <li className="trusted-group">
+          <img src="_" alt="" />
+          <figcaption>... Badminton</figcaption>
+        </li>
+      </ul>
     </section>
-  )
+  );
 }
