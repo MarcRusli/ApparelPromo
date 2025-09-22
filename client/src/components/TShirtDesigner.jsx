2c0bbc433a44e33ba4c9a6d411243476f6452f18
amisuta <rusli.marc@gmail.com>
2025-09-22 00:57:41 -0700

    Add basic T-shirt front-back switching feature

--- client/src/components/TShirtDesigner.jsx
+++ client/src/components/TShirtDesigner.jsx
@@ -5,64 +5,105 @@ import Toolbar from "./Toolbar";
 import "./TShirtDesigner.css";
 
 export default function TShirtDesigner() {
-  const canvasRef = useRef(null);
-  const [canvas, setCanvas] = useState(null);
+  const canvasFrontRef = useRef(null);
+  const canvasBackRef = useRef(null);
+  const [activeCanvas, setActiveCanvas] = useState(null);
+  const fabricCanvasFront = useRef(null);
+  const fabricCanvasBack = useRef(null);
+
+  const handleToggleSide = () => {
+    setActiveCanvas((prev) =>
+      prev == fabricCanvasFront.current
+        ? fabricCanvasBack.current
+        : fabricCanvasFront.current
+    );
+  };
 
   // do once on mount
   useEffect(() => {
-    if (canvasRef.current) {
-      const initCanvas = new Canvas(canvasRef.current, {
-        width: Math.min(window.innerWidth * 0.8, 800),
-        height: Math.min(window.innerHeight * 0.8, 800),
-      });
-
-      initCanvas.backgroundColor = "#fff";
-
-      // add non-interactive T-shirt background
-      // --- Preload background image (non-interactive) ---
-      FabricImage.fromURL(
-        "/t-shirt-background.jpg"
-        //"https://cdnp.sanmar.com/medias/sys_master/images/hf9/h1b/28963219472414/1200W_4349_Black-0-ST350BlackModelFront5/1200W-4349-Black-0-ST350BlackModelFront5.jpg"
-      ).then((img) => {
-        const maxWidth = initCanvas.getWidth();
-        const maxHeight = initCanvas.getHeight();
-
-        const origW = img.get("width");
-        const origH = img.get("height");
-        const scaleX = maxWidth / origW;
-        const scaleY = maxHeight / origH;
-        const scale = Math.min(scaleX, scaleY);
-
-        img.set({
-          scaleX: scale,
-          scaleY: scale,
-          originX: "center",
-          originY: "center",
-          selectable: false, // lock it
-          evented: false, // no mouse events
+    const setUpCanvas = (canvasRef, shirtURL) => {
+      if (canvasRef.current) {
+        const initCanvas = new Canvas(canvasRef.current, {
+          width: Math.min(window.innerWidth * 0.8, 800),
+          height: Math.min(window.innerHeight * 0.8, 800),
         });
 
-        initCanvas.add(img);
-        initCanvas.centerObject(img);
+        initCanvas.backgroundColor = "#fff";
 
-        // Send it to the very back so all new objects are above
-        initCanvas.requestRenderAll();
+        // add non-interactive T-shirt background
+        // --- Preload background image (non-interactive) ---
+        FabricImage.fromURL(shirtURL).then((img) => {
+          const maxWidth = initCanvas.getWidth();
+          const maxHeight = initCanvas.getHeight();
 
-        setCanvas(initCanvas);
-      });
+          const origW = img.get("width");
+          const origH = img.get("height");
+          const scaleX = maxWidth / origW;
+          const scaleY = maxHeight / origH;
+          const scale = Math.min(scaleX, scaleY);
 
-      //initCanvas.renderAll();
+          img.set({
+            scaleX: scale,
+            scaleY: scale,
+            originX: "center",
+            originY: "center",
+            selectable: false, // lock it
+            evented: false, // no mouse events
+          });
 
-      return () => {
-        initCanvas.dispose();
-      };
-    }
+          initCanvas.add(img);
+          initCanvas.centerObject(img);
+          initCanvas.requestRenderAll();
+
+          //setCanvas(initCanvas);
+        });
+
+        //initCanvas.renderAll();
+
+        return initCanvas;
+      }
+    };
+
+    fabricCanvasFront.current = setUpCanvas(
+      canvasFrontRef,
+      "/t-shirt-background-front.jpg"
+    );
+    fabricCanvasBack.current = setUpCanvas(
+      canvasBackRef,
+      "/t-shirt-background-back.jpg"
+    );
+    setActiveCanvas(fabricCanvasFront.current);
+
+    return () => {
+      fabricCanvasFront.current.dispose();
+      fabricCanvasBack.current.dispose();
+    };
   }, []);
 
   return (
     <div className="designer-container">
-      <Toolbar canvas={canvas} />
-      <canvas id="canvas" ref={canvasRef} />
+      <Toolbar canvas={activeCanvas} />
+      <div
+        style={{
+          border: "1px solid #ccc",
+          display:
+            activeCanvas === fabricCanvasFront.current ? "block" : "none",
+        }}
+      >
+        <canvas className="canvas canvas-front" ref={canvasFrontRef} />
+      </div>
+      <div
+        style={{
+          border: "1px solid #ccc",
+          display:
+            activeCanvas === fabricCanvasBack.current ? "block" : "none",
+        }}
+      >
+        <canvas className="canvas canvas-back" ref={canvasBackRef} />
+      </div>
+      <button onClick={handleToggleSide}>
+        Switch to {activeCanvas == fabricCanvasFront.current ? "Back" : "Front"}
+      </button>
     </div>
   );
 }
